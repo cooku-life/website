f2e57867c7216ae8c2f8ed959e052a7d7436d005
Seimo <seimo@seimo.cn>
2025-04-30 16:40:50 +0800

    fix: update path for docs directory in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     viteStaticCopy({ // 配置插件
       targets: [
         {
-          src: 'docs/**/*', // 复制 docs 目录下所有文件和子目录
+          src: 'public/docs/**/*', // 复制 docs 目录下所有文件和子目录
           dest: 'docs'     // 复制到输出目录下的 docs 文件夹
         }
       ]
